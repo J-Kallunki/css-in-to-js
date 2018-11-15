12cb390060ce3dfe8d2d671b0985b145ee583e57
Joonas Kallunki <jkallunki@gmail.com>
2018-11-15 09:17:15 +0200

    Update tests

--- src/cssintojs.test.js
+++ src/cssintojs.test.js
@@ -1,68 +1,74 @@
 import { expect } from 'chai';
 import { cssToJs, minifyCss } from './cssintojs';
 
-const css = `html {
-  line-height: 1.15; /* 1 */
-  -webkit-text-size-adjust: 100%; /* 2 */
-}
-/* Comments
-========================================================================== */
-
-/**
- * Another comment
- */
-button::-moz-focus-inner,
-[type="button"]::-moz-focus-inner,
-[type="reset"]::-moz-focus-inner,
-[type="submit"]::-moz-focus-inner {
-  border-style: none;
-  padding: 0;
-}`;
 
 describe('csstocssinjs', () => {
-  const resets = cssToJs(css);
-  const resets2 = cssToJs(css, { cssToString: true });
+  const css = `html {
+    line-height: 1.15; /* 1 */
+    -webkit-text-size-adjust: 100%; /* 2 */
+  }
+  /* Comments
+  ========================================================================== */
 
-  it('Should be converted to an object', () => {
-    expect(resets).to.be.a('object');
-    expect(resets2).to.be.a('object');
-  })
+  /**
+   * Another comment
+   */
+  button::-moz-focus-inner,
+  [type="button"]::-moz-focus-inner,
+  [type="reset"]::-moz-focus-inner,
+  [type="submit"]::-moz-focus-inner {
+    border-style: none;
+    padding: 0;
+  }`;
 
-  it('Should contain selectors', () => {
-    const keys = Object.keys(resets);
-    const keys2 = Object.keys(resets2);
+  describe('cssToJs', () => {
+    const resets = cssToJs(css);
+    const resets2 = cssToJs(css, { cssToString: true });
 
-    expect(keys).to.be.lengthOf(5);
-    expect(keys2).to.be.lengthOf(5);
+    it('Should be converted to an object', () => {
+      expect(resets).to.be.a('object');
+      expect(resets2).to.be.a('object');
+    })
 
-    expect(keys[3]).to.equal('[type="reset"]::-moz-focus-inner');
-    expect(keys2[3]).to.equal('[type="reset"]::-moz-focus-inner');
-  })
+    it('Should contain selectors', () => {
+      const selectors = Object.keys(resets);
+      const selectors2 = Object.keys(resets2);
 
-  it('Should contain declarations', () => {
-    const values = resets['html'];
-    const values2 = resets2['html'];
+      expect(selectors).to.be.lengthOf(5);
+      expect(selectors2).to.be.lengthOf(5);
 
-    expect(Object.keys(values)).to.be.lengthOf(2);
-    expect(values['lineHeight']).to.equal('1.15');
+      expect(selectors).to.include('html');
+      expect(selectors2).to.include('html');
+      expect(selectors[3]).to.equal('[type="reset"]::-moz-focus-inner');
+      expect(selectors2[3]).to.equal('[type="reset"]::-moz-focus-inner');
+    })
 
-    expect(values2).to.be.a('string');
-    expect(values2).to.equal('line-height: 1.15; -webkit-text-size-adjust: 100%;');
+    it('Should contain declarations', () => {
+      const declarations = resets['html'];
+      const declarations2 = resets2['html'];
 
-  })
+      expect(Object.keys(declarations)).to.be.lengthOf(2);
+      expect(declarations['lineHeight']).to.equal('1.15');
 
-  it('Should need selector', () => {
-    const noSelector = cssToJs(`line-height: 1.15; /* 1 */`);
+      expect(declarations2).to.be.a('string');
+      expect(declarations2).to.equal('line-height: 1.15; -webkit-text-size-adjust: 100%;');
 
-    expect(noSelector).to.equal(undefined);
-  })
-});
+    })
 
-describe('minifyCss', () => {
-  const minify = minifyCss(css);
+    it('Should need selector', () => {
+      const noSelector = cssToJs(`line-height: 1.15; /* 1 */`);
 
-  it('Should still be a string', () => {
-    expect(minify).to.be.a('string');
-  })
+      expect(noSelector).to.equal(undefined);
+    })
+  });
+
+  describe('minifyCss', () => {
+    const minify = minifyCss(css);
+
+    it('Should still be a string', () => {
+      expect(minify).to.be.a('string');
+    })
+
+  });
 
 });
