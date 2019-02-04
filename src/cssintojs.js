68547ca4c28db1f8302d12556e9ae29f7b613aec
Joonas Kallunki <jkallunki@gmail.com>
2019-02-04 07:49:01 +0200

    Replace clean-css with more simple implementation

--- src/cssintojs.js
+++ src/cssintojs.js
@@ -1,30 +1,37 @@
-import postcss from "postcss";
-import CleanCSS from "clean-css";
-import camelCase from "lodash/camelCase";
-import upperFirst from "lodash/upperFirst";
+import postcss from 'postcss';
+import camelCase from 'lodash/camelCase';
+import upperFirst from 'lodash/upperFirst';
 const _ = { camelCase, upperFirst };
 
 export const minifyCss = cssString =>
-  new CleanCSS({ compatibility: "*" }).minify(cssString).styles;
+  cssString
+    .replace(/\r?\n|\r/g, '') // Remove line breaks
+    .replace(/(\|\\r|\	|\\f|\\v)+/g, '') // Remove tabs etc.
+    .replace(/\/\*[\s\S]*?\*\/|([^\\:]|^)\/\/.*$/gm, '$1') // Remove comments.
+    .replace(/ +/g, ' ') // Remove redundant whitespace.
+    .replace(/ *(:|;|{|}) */g, '$1'); // Remove spaces around those characters
 
 export const cssToJs = (cssString, { cssToString = false } = {}) => {
   const root = postcss.parse(cssString, {});
-  const rootRules = !!root && !!root.nodes && root.nodes.filter(node => node.type === "rule");
+  const rootRules =
+    !!root && !!root.nodes && root.nodes.filter(node => node.type === 'rule');
 
   if (!rootRules || rootRules.length < 1) {
-    if (console) console.error("No CSS selectors found");
+    if (console) console.error('No CSS selectors found');
     return undefined;
   }
 
   return Object.entries(rootRules).reduce((acc, [key, { selector, nodes }]) => {
     const formatToObject = prop => {
       const camelProp = _.camelCase(prop);
-      return prop.startsWith("-") ? _.upperFirst(camelProp) : camelProp;
+      return prop.startsWith('-') ? _.upperFirst(camelProp) : camelProp;
     };
-    const selectors = !!selector ? selector.split(",").map(s => s.trim()): [];
-    const cssDeclarations = !!nodes ? nodes.filter(node => node.type === "decl") : [];
+    const selectors = !!selector ? selector.split(',').map(s => s.trim()) : [];
+    const cssDeclarations = !!nodes
+      ? nodes.filter(node => node.type === 'decl')
+      : [];
     const cssDeclarationsFormatted = !!cssToString
-      ? cssDeclarations.map(({ prop, value }) => `${prop}: ${value};`).join(" ")
+      ? cssDeclarations.map(({ prop, value }) => `${prop}: ${value};`).join(' ')
       : cssDeclarations.reduce(
           (accDecl, { prop, value }) => ({
             ...accDecl,
